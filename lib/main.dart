2338128214135b2f835e62d8b53cbc176d15bdc7
Alisson Filipe <alifdas10@gmail.com>
2023-12-13 16:48:42 -0300

    colocando os elementos no topo

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,21 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   double saldo = 0;
   double dividaCartao = 0;
   double principalFonteRenda = 0;
@@ -32,114 +41,122 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Gerenciador Financeiro'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                buildCampo(
-                  'Saldo Atual: \$${saldo.toStringAsFixed(2)}',
-                  saldoController,
-                  saldoFocus,
-                  'Novo Saldo',
-                      () {
-                    setState(() {
-                      saldo = double.tryParse(saldoController.text) ?? saldo;
-                      editandoSaldo = false;
-                    });
-                  },
-                  editandoSaldo,
-                      () {
-                    setState(() {
-                      editandoSaldo = !editandoSaldo;
-                    });
-                    _gerenciarFoco(editandoSaldo, saldoFocus);
-                  },
-                ),
-                SizedBox(height: 16),
-                buildCampo(
-                  'Dívida do Cartão de Crédito: \$${dividaCartao.toStringAsFixed(2)}',
-                  dividaCartaoController,
-                  dividaCartaoFocus,
-                  'Nova Dívida do Cartão de Crédito',
-                      () {
-                    setState(() {
-                      dividaCartao = double.tryParse(dividaCartaoController.text) ?? dividaCartao;
-                      editandoDividaCartao = false;
-                    });
-                  },
-                  editandoDividaCartao,
-                      () {
-                    setState(() {
-                      editandoDividaCartao = !editandoDividaCartao;
-                    });
-                    _gerenciarFoco(editandoDividaCartao, dividaCartaoFocus);
-                  },
-                ),
-                SizedBox(height: 16),
-                buildCampo(
-                  'Principal Fonte de Renda: \$${principalFonteRenda.toStringAsFixed(2)}',
-                  principalFonteRendaController,
-                  principalFonteRendaFocus,
-                  'Alterar Fonte de Renda',
-                      () {
-                    setState(() {
-                      principalFonteRenda =
-                          double.tryParse(principalFonteRendaController.text) ?? principalFonteRenda;
-                      editandoPrincipalFonteRenda = false;
-                    });
-                  },
-                  editandoPrincipalFonteRenda,
-                      () {
-                    setState(() {
-                      editandoPrincipalFonteRenda = !editandoPrincipalFonteRenda;
-                    });
-                    _gerenciarFoco(editandoPrincipalFonteRenda, principalFonteRendaFocus);
-                  },
-                ),
-                SizedBox(height: 16),
-                buildCampo(
-                  'Data da Principal Fonte de Renda: ${_formatarData(dataPrincipalFonteRendaController.text)}',
-                  dataPrincipalFonteRendaController,
-                  dataPrincipalFonteRendaFocus,
-                  'Alterar Data',
-                      () {
-                    setState(() {
-                      editandoDataPrincipalFonteRenda = false;
-                    });
-                  },
-                  editandoDataPrincipalFonteRenda,
-                      () {
-                    setState(() {
-                      editandoDataPrincipalFonteRenda = !editandoDataPrincipalFonteRenda;
-                      dataPrincipalFonteRendaController.text =
-                          _formatarData(dataPrincipalFonteRendaController.text);
-                    });
-                    _gerenciarFoco(editandoDataPrincipalFonteRenda, dataPrincipalFonteRendaFocus);
-                  },
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      resultadoSubtracao = saldo - dividaCartao;
-                    });
-                  },
-                  child: Text('Subtrair Dívida do Cartão'),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'O que você realmente tem: \$${resultadoSubtracao.toStringAsFixed(2)}',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gerenciador Financeiro'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              buildCampo(
+                'Saldo Atual: \$${saldo.toStringAsFixed(2)}',
+                saldoController,
+                saldoFocus,
+                'Novo Saldo',
+                    () {
+                  setState(() {
+                    saldo = double.tryParse(saldoController.text) ?? saldo;
+                    editandoSaldo = false;
+                  });
+                },
+                editandoSaldo,
+                    () {
+                  setState(() {
+                    editandoSaldo = !editandoSaldo;
+                  });
+                  _gerenciarFoco(editandoSaldo, saldoFocus);
+                },
+              ),
+              SizedBox(height: 16),
+              buildCampo(
+                'Dívida do Cartão de Crédito: \$${dividaCartao.toStringAsFixed(2)}',
+                dividaCartaoController,
+                dividaCartaoFocus,
+                'Nova Dívida do Cartão de Crédito',
+                    () {
+                  setState(() {
+                    dividaCartao = double.tryParse(dividaCartaoController.text) ?? dividaCartao;
+                    editandoDividaCartao = false;
+                  });
+                },
+                editandoDividaCartao,
+                    () {
+                  setState(() {
+                    editandoDividaCartao = !editandoDividaCartao;
+                  });
+                  _gerenciarFoco(editandoDividaCartao, dividaCartaoFocus);
+                },
+              ),
+              SizedBox(height: 16),
+              buildCampo(
+                'Principal Fonte de Renda: \$${principalFonteRenda.toStringAsFixed(2)}',
+                principalFonteRendaController,
+                principalFonteRendaFocus,
+                'Alterar Fonte de Renda',
+                    () {
+                  setState(() {
+                    principalFonteRenda =
+                        double.tryParse(principalFonteRendaController.text) ?? principalFonteRenda;
+                    editandoPrincipalFonteRenda = false;
+                  });
+                },
+                editandoPrincipalFonteRenda,
+                    () {
+                  setState(() {
+                    editandoPrincipalFonteRenda = !editandoPrincipalFonteRenda;
+                  });
+                  _gerenciarFoco(editandoPrincipalFonteRenda, principalFonteRendaFocus);
+                },
+              ),
+              SizedBox(height: 16),
+              buildCampo(
+                'Data da Principal Fonte de Renda: ${_formatarData(dataPrincipalFonteRendaController.text)}',
+                dataPrincipalFonteRendaController,
+                dataPrincipalFonteRendaFocus,
+                'Alterar Data',
+                    () {
+                  setState(() {
+                    editandoDataPrincipalFonteRenda = false;
+                  });
+                },
+                editandoDataPrincipalFonteRenda,
+                    () {
+                  setState(() {
+                    editandoDataPrincipalFonteRenda = !editandoDataPrincipalFonteRenda;
+                    dataPrincipalFonteRendaController.text =
+                        _formatarData(dataPrincipalFonteRendaController.text);
+                  });
+                  _gerenciarFoco(editandoDataPrincipalFonteRenda, dataPrincipalFonteRendaFocus);
+                },
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    resultadoSubtracao = saldo - dividaCartao;
+                  });
+                },
+                child: Text('Subtrair Dívida do Cartão'),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'O que você realmente tem: \$${resultadoSubtracao.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AtivosScreen()),
+                  );
+                },
+                child: Text('Ver Ativos e outras dívidas'),
+              ),
+            ],
           ),
         ),
       ),
@@ -195,12 +212,31 @@ class _MyAppState extends State<MyApp> {
     } else {
       FocusScope.of(context).requestFocus(FocusNode());
     }
-  }//teste
+  }
 
   String _formatarData(String data) {
     if (data.length == 6) {
       return '${data.substring(0, 2)}/${data.substring(2, 4)}/${data.substring(4)}';
     }
     return data;
+  }
+}
+
+class AtivosScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ativos e dívidas'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Voltar'),
+        ),
+      ),
+    );
   }
 }
